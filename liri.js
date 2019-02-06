7c7b724165990f6e9e9e9411ed198c7ba96fe1e8
Dennis Ries <dtries@gmail.com>
2019-02-05 22:41:53 -0700

    Movies and Concerts Finished, Spotify and random need work

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 var moment = require("moment");
@@ -13,60 +12,96 @@ var whichSearch=process.argv[2];
 
 var artist = "";
 
+var movie = "";
+
 var theBand;
 
 switch (whichSearch) {
     case "concert-this":
 
+        for (var i = 3; i < nodeArgs.length; i++) {
 
-
-    for (var i = 3; i < nodeArgs.length; i++) {
-
-        if (i > 3 && i < nodeArgs.length) {
-        artist = artist + "+" + nodeArgs[i];
-        }
-        else {
-        artist += nodeArgs[i];
-    
-        }
-    }
-
-
-
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(bandsResponse) {
-            // theBand = JSON.stringify(bandsResponse);
-            // console.log(bandsResponse);
-
-            for (var i=0; i<bandsResponse.data.length; i++) {
-            
-            console.log(" ");
-            console.log("Event " + [i+1] + " of "+ bandsResponse.data.length)    
-            console.log("-----------------------------------------");    
-            console.log("Venue: " +bandsResponse.data[i].venue.name);
-            console.log("Location: "+bandsResponse.data[i].venue.city+","+bandsResponse.data[i].venue.country);
-            convertedDate = moment(bandsResponse.data[i].datetime).format("MM DD YYYY");
-            console.log("Date: "+convertedDate);
-            console.log("-----------------------------------------");
-
+            if (i > 3 && i < nodeArgs.length) {
+            artist = artist + "+" + nodeArgs[i];
             }
-            
+            else {
+            artist += nodeArgs[i];
+        
+            }
         }
-    );
-    break;
+
+
+
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+            function(bandsResponse) {
+
+                for (var i=0; i<bandsResponse.data.length; i++) {
+                
+                console.log(" ");
+                console.log("Event " + [i+1] + " of "+ bandsResponse.data.length)    
+                console.log("-----------------------------------------");    
+                console.log("Venue: " +bandsResponse.data[i].venue.name);
+                console.log("Location: "+bandsResponse.data[i].venue.city+","+bandsResponse.data[i].venue.country);
+                convertedDate = moment(bandsResponse.data[i].datetime).format("MM DD YYYY");
+                console.log("Date: "+convertedDate);
+                console.log("-----------------------------------------");
+
+                }
+                
+            }
+        );
+        break;
 
     case "spotify-this-song":
         console.log("Made it to Spotify");
         break;
 
     case "movie-this":
-        console.log("Made it to movie search");
-        axios.get(" http://www.omdbapi.com/?apikey=trilogy&t=batman")// + movie)
+
+        if (nodeArgs.length < 4) {
+            movie = "Mr+Nobody";
+            console.log(movie);
+        }
+
+        else {
+            for (var i = 3; i < nodeArgs.length; i++) {
+
+                if (i > 3 && i < nodeArgs.length) {
+                movie = movie + "+" + nodeArgs[i];
+                }
+                else {
+                movie += nodeArgs[i];
+            
+                }
+            }
+        }
+
+
+
+        console.log("the movie title is: "+ movie)
+
+        axios.get(" http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
         .then(
             function(movieResponse) {
-                console.log(movieResponse);
+
+                console.log(" ");
+                console.log("-----------------------------------------");
+                console.log("Title: " + movieResponse.data.Title +"\n");
+                console.log("Year Released: " + movieResponse.data.Year + "\n");
+                console.log(movieResponse.data.Ratings[0].Source + " Rating: " + movieResponse.data.Ratings[0].Value + "\n");
+                console.log(movieResponse.data.Ratings[1].Source + " Rating: " + movieResponse.data.Ratings[1].Value + "\n");
+                console.log("Production Country(ies): " + movieResponse.data.Country + "\n");
+                console.log("Language(s): " + movieResponse.data.Language + "\n");
+                console.log("Plot: " + movieResponse.data.Plot + "\n");
+                console.log("Actors: " + movieResponse.data.Actors + "\n");
+                console.log("-----------------------------------------");
             }
-        );
+            
+        )
+
+        .catch(function () {
+            console.log("\nMOVIE TITLE CANNOT BE FOUND OR IS INCOMPLETE. Please enter again.");
+        });
 
        
         break;
@@ -75,6 +110,6 @@ switch (whichSearch) {
         console.log("Made it to do what it says");
         break;
     default:
-        console.log("Search cannot be completed as entered, try again")
+      
 }
 
