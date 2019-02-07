262a97b9a53e702106a6458b0badd7ff5e776792
Dennis Ries <dtries@gmail.com>
2019-02-06 17:27:23 -0700

    All searches work

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
-console.log('this is loaded');
-
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.SPOTIFY_ID, //"ba94d0b079344189a4865f4cb8dec70e",
+    secret: process.env.SPOTIFY_SECRET //"ffa508c92dcc48dfa81502f8168174e6"
 };
--- liri.js
+++ liri.js
@@ -1,92 +1,96 @@
 require("dotenv").config();
 
+var keys = require("./keys.js");
+
 var moment = require("moment");
 
-// var spotify=new spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
 
 var axios = require("axios");
 
+var fs = require("fs");
+
 var nodeArgs = process.argv;
 
-var whichSearch=process.argv[2];
+var whichSearch = process.argv[2];
 
 var artist = "";
 
 var movie = "";
 
-var theBand;
+var song = "";
 
-switch (whichSearch) {
-    case "concert-this":
+var commandLine = "";
 
+pickSearch(whichSearch);
+
+function pickSearch(whichSearch) {
+
+    switch (whichSearch) {
+
+        case "concert-this":
+
+            concertThis();
+
+            break;
+
+        case "spotify-this-song":
+
+            musicThis();
+
+            break;
+
+        case "movie-this":
+
+            movieThis();
+
+            break;
+
+        case "do-what-it-says":
+
+            fs.readFile('random.txt', 'utf8', function (err, data) {
+                if (err) throw err;
+                commandLine = data.split(",");
+
+                nodeArgs.splice(2, 1, commandLine[0],commandLine[1]);
+
+                whichSearch = commandLine[0];
+
+                pickSearch(whichSearch);
+
+            });
+            break;
+
+        default:
+            console.log("LIRI cannot process your request. Please check your entry and try again.");
+
+    }
+}
+
+function movieThis() {
+    if (nodeArgs.length < 4) {
+        movie = "Mr+Nobody";
+    } else {
         for (var i = 3; i < nodeArgs.length; i++) {
 
             if (i > 3 && i < nodeArgs.length) {
-            artist = artist + "+" + nodeArgs[i];
-            }
-            else {
-            artist += nodeArgs[i];
-        
-            }
-        }
-
-
-
-        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-            function(bandsResponse) {
-
-                for (var i=0; i<bandsResponse.data.length; i++) {
-                
-                console.log(" ");
-                console.log("Event " + [i+1] + " of "+ bandsResponse.data.length)    
-                console.log("-----------------------------------------");    
-                console.log("Venue: " +bandsResponse.data[i].venue.name);
-                console.log("Location: "+bandsResponse.data[i].venue.city+","+bandsResponse.data[i].venue.country);
-                convertedDate = moment(bandsResponse.data[i].datetime).format("MM DD YYYY");
-                console.log("Date: "+convertedDate);
-                console.log("-----------------------------------------");
-
-                }
-                
-            }
-        );
-        break;
-
-    case "spotify-this-song":
-        console.log("Made it to Spotify");
-        break;
-
-    case "movie-this":
-
-        if (nodeArgs.length < 4) {
-            movie = "Mr+Nobody";
-            console.log(movie);
-        }
-
-        else {
-            for (var i = 3; i < nodeArgs.length; i++) {
-
-                if (i > 3 && i < nodeArgs.length) {
                 movie = movie + "+" + nodeArgs[i];
-                }
-                else {
+            } else {
                 movie += nodeArgs[i];
-            
-                }
+
             }
         }
+    }
 
 
-
-        console.log("the movie title is: "+ movie)
-
-        axios.get(" http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+    axios.get(" http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
         .then(
-            function(movieResponse) {
-
+            
+            function (movieResponse) {
+                
                 console.log(" ");
                 console.log("-----------------------------------------");
-                console.log("Title: " + movieResponse.data.Title +"\n");
+                console.log("Title: " + movieResponse.data.Title + "\n");
                 console.log("Year Released: " + movieResponse.data.Year + "\n");
                 console.log(movieResponse.data.Ratings[0].Source + " Rating: " + movieResponse.data.Ratings[0].Value + "\n");
                 console.log(movieResponse.data.Ratings[1].Source + " Rating: " + movieResponse.data.Ratings[1].Value + "\n");
@@ -95,21 +99,107 @@ switch (whichSearch) {
                 console.log("Plot: " + movieResponse.data.Plot + "\n");
                 console.log("Actors: " + movieResponse.data.Actors + "\n");
                 console.log("-----------------------------------------");
+
             }
-            
+
         )
 
         .catch(function () {
             console.log("\nMOVIE TITLE CANNOT BE FOUND OR IS INCOMPLETE. Please enter again.");
         });
 
-       
-        break;
-    
-    case "do-what-it-says":
-        console.log("Made it to do what it says");
-        break;
-    default:
-      
 }
 
+function musicThis() {
+
+    if (nodeArgs.length < 4) {
+        song = "The+Sign+Ace+of+Base";
+        console.log(song);
+        type = 'artist AND track'
+    } else {
+        for (var i = 3; i < nodeArgs.length; i++) {
+
+            if (i > 3 && i < nodeArgs.length) {
+                song = song + "+" + nodeArgs[i];
+            } else {
+                song += nodeArgs[i];
+
+            }
+        }
+    }
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({
+        type: 'track',
+        query: song,
+        limit: 10
+    }, function (err, songResponse) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        for (var i = 0; i < songResponse.tracks.items.length; i++) {
+
+            if (songResponse.tracks.items[i].preview_url === null) {
+                previewURL = "Song Preview Not Available";
+            } else {
+                previewURL = songResponse.tracks.items[i].preview_url;
+            }
+
+            console.log(" ");
+            console.log("Track " + [i + 1] + " of " + songResponse.tracks.items.length);
+
+            console.log(" ");
+            console.log("-----------------------------------------");
+            console.log("Artist(s): " + songResponse.tracks.items[i].artists[0].name + "\n"); //.name +"\n");
+            console.log("Song Title: " + songResponse.tracks.items[i].name + "\n");
+            console.log("Preview Link: " + previewURL + "\n");
+            console.log("Album: " + songResponse.tracks.items[i].album.name + "\n");
+            console.log("-----------------------------------------");
+            console.log("-----------------------------------------");
+
+        }
+
+
+    });
+}
+
+function concertThis() {
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            artist = artist + "+" + nodeArgs[i];
+        } else {
+            artist += nodeArgs[i];
+
+        }
+    }
+
+
+
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (bandsResponse) {
+
+            for (var i = 0; i < bandsResponse.data.length; i++) {
+
+                console.log(" ");
+                console.log("Event " + [i + 1] + " of " + bandsResponse.data.length)
+                console.log("-----------------------------------------");
+                console.log("Venue: " + bandsResponse.data[i].venue.name);
+                console.log("Location: " + bandsResponse.data[i].venue.city + "," + bandsResponse.data[i].venue.country);
+                convertedDate = moment(bandsResponse.data[i].datetime).format("MM DD YYYY");
+                console.log("Date: " + convertedDate);
+                console.log("-----------------------------------------");
+                console.log("-----------------------------------------");
+
+            }
+
+        }
+        
+    )
+
+    .catch(function () {
+        console.log("\nA CONCERT EVENT CANNOT BE FOUND OR THE SEARCH TERM(S) IS/ARE INCORRECT. Please check your entry and try again.");
+    });
+}
